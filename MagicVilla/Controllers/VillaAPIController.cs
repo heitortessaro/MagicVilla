5c6aa74000d45280abd66c883a793e18fe5048e3
Heitor <heitor.j.tessaro@gmail.com>
2023-02-01 20:44:10 -0300

    feat: add the first GET endpoint with static return

--- MagicVilla/Controllers/VillaAPIController.cs
+++ MagicVilla/Controllers/VillaAPIController.cs
@@ -1,10 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaAPI.Models;
 
-namespace magicVilla_Villa.Controllers
+namespace MagicVilla_VillaAPI.Controllers
 {
   [ApiController]
+  [Route("[controller]")]
   public class VillaAPICOntroller : ControllerBase
   {
-
+    [HttpGet]
+    public IEnumerable<Villa> GetVillas()
+    {
+      return new List<Villa> {
+        new Villa{Id=1,Name="Pool View"},
+        new Villa{Id=2,Name="Beach View"},
+      };
+    }
   }
 }
